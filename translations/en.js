e325a3dbeefb9cc687e8de60fe11084e86c65444
cintiaMahjong <cintiahor@gmail.com>
2026-09-04 09:40:56 +0200

    Add files via upload

---
+++ translations/en.js
@@ -0,0 +1,223 @@
+const en = {
+  language: "Language",
+  spanish: "Español",
+  english: "English",
+  chinese: "中文",
+
+  appTitle: "Mahjong Madrid",
+  appSubtitle: "Game manager · 4 or 5 players",
+
+  continueGame: "Continue game",
+  newGame: "New game",
+  history: "Game history",
+
+  newGameTitle: "New game",
+  numberOfPlayers: "Number of players",
+  fourPlayers: "4 players",
+  fivePlayers: "5 players",
+
+  player: "Player",
+  playerName: "Player name",
+  enterPlayerName: "Enter name",
+  enterPlayers: "Enter the players' names",
+  emptyPlayerNames:
+    "You must enter the name of every player.",
+
+  startGame: "Start game",
+  back: "Back",
+
+  round: "Round",
+  hand: "Hand",
+  of: "of",
+
+  registerHand: "Register hand",
+  undoHand: "Undo hand",
+  saveAndExit: "Save and exit",
+  finishGame: "Finish game",
+
+  wall: "Self-draw",
+  discard: "Discard",
+  draw: "Draw",
+
+  winner: "Winner",
+  loser: "Loser",
+  discarder: "Discarding player",
+
+  points: "Points",
+  handPoints: "Hand points",
+  minimum8: "Minimum 8 points",
+
+  enterPoints: "Enter the points.",
+  pointsMustBeNumber:
+    "Points must be a number.",
+  minimumPoints:
+    "The hand must have at least 8 points.",
+
+  invalidPointsTitle:
+    "Invalid points",
+
+  accept:
+    "Accept",
+
+  confirm: "Confirm",
+  cancel: "Cancel",
+  next: "Next",
+  previous: "Previous",
+
+  registerHandTitle:
+    "Register hand",
+
+  mahjong:
+    "Mahjong",
+
+  whoWon:
+    "Who won?",
+
+  howWon:
+    "How did they win?",
+
+  whoDiscarded:
+    "Who discarded?",
+
+  saveHand:
+    "Save hand",
+
+  activeGameTitle:
+    "YOU HAVE A GAME IN PROGRESS",
+
+  activeGameMessage:
+    "You already have a saved game.\nWhat would you like to do?",
+
+  cancelAndNewGame:
+    "Cancel and start new game",
+
+  noSavedGame:
+    "There is no saved game.",
+
+  gameSavedMessage:
+    "The game will be saved and you can continue later.",
+
+  undoConfirmation:
+    "Are you sure you want to undo the last hand?",
+
+  undoMessage:
+    "The points from the last hand will be removed and you can register it again.",
+
+  noHandsToUndo:
+    "There are no hands to undo.",
+
+  finishGameQuestion:
+    "Finish game?",
+
+  finishGameMessage:
+    "The game will be marked as finished and added to the history.",
+
+  gameFinished:
+    "Game Finished",
+
+  gameFinishedMessage:
+    "The game has been successfully finished.",
+
+  cannotRegisterFinished:
+    "The game has already finished. No more hands can be registered.",
+
+  cannotSaveGame:
+    "The game could not be saved.",
+
+  cannotSaveHistory:
+    "The game could not be saved to the history.",
+
+  windChange:
+    "WIND CHANGE",
+
+  roundChange:
+    "Changing to Round",
+
+  newPositions:
+    "New positions",
+
+  windSouth:
+    "South Wind",
+
+  windWest:
+    "West Wind",
+
+  windNorth:
+    "North Wind",
+
+  windEast:
+    "East Wind",
+
+  east: "EAST",
+  south: "SOUTH",
+  west: "WEST",
+  north: "NORTH",
+  na: "N/A",
+
+  continueButton:
+    "Continue",
+
+  rest:
+    "REST",
+
+  inactivePlayer:
+    "Inactive player",
+
+  results:
+    "Results",
+
+  ranking:
+    "Ranking",
+
+  winnerLabel:
+    "Winner",
+
+  first: "1st",
+  second: "2nd",
+  third: "3rd",
+  fourth: "4th",
+
+  playedHands:
+    "Hands played",
+
+  noHandsRegistered:
+    "No hands registered.",
+
+  backToHistory:
+    "Back to history",
+
+  historyTitle:
+    "Game history",
+
+  noFinishedGames:
+    "There are no finished games.",
+
+  game:
+    "Game",
+
+  players:
+    "Players",
+
+  playersUnavailable:
+    "Inactive players",
+
+  noPlayerData:
+    "No player data",
+
+  viewGame:
+    "View game",
+
+  deleteGame:
+    "Delete",
+
+  deleteGameConfirmation:
+    "Are you sure you want to delete this game from the history?",
+
+  gameDate:
+    "Date",
+
+  copyright:
+    "© 2026 C. Horcajo · All rights reserved."
+};
+
+export default en;
---
+++ translations/zh.js
@@ -0,0 +1,226 @@
+const zh = {
+  language: "语言",
+  spanish: "西班牙语",
+  english: "英语",
+  chinese: "中文",
+
+  appTitle: "马德里麻将",
+  appSubtitle: "麻将对局管理器 · 4或5名玩家",
+
+  continueGame: "继续对局",
+  newGame: "新对局",
+  history: "对局历史",
+
+  newGameTitle: "新对局",
+  numberOfPlayers: "玩家人数",
+  fourPlayers: "4名玩家",
+  fivePlayers: "5名玩家",
+
+  player: "玩家",
+  playerName: "玩家姓名",
+  enterPlayerName: "输入姓名",
+  enterPlayers: "请输入所有玩家的姓名",
+  emptyPlayerNames:
+    "请输入所有玩家的姓名。",
+
+  startGame: "开始对局",
+  back: "返回",
+
+  round: "圈",
+  hand: "局",
+  of: "/",
+
+  registerHand: "记录本局",
+  undoHand: "撤销本局",
+  saveAndExit: "保存并退出",
+  finishGame: "结束对局",
+
+  wall: "自摸",
+  discard: "荣和",
+  draw: "流局",
+
+  winner: "赢家",
+  loser: "输家",
+  discarder: "放铳玩家",
+
+  points: "分",
+  handPoints: "本局分数",
+  minimum8: "最低8分",
+
+  enterPoints:
+    "请输入分数。",
+
+  pointsMustBeNumber:
+    "分数必须是数字。",
+
+  minimumPoints:
+    "本局至少需要8分。",
+
+  invalidPointsTitle:
+    "分数无效",
+
+  accept:
+    "确定",
+
+  confirm: "确认",
+  cancel: "取消",
+  next: "下一步",
+  previous: "上一步",
+
+  registerHandTitle:
+    "记录本局",
+
+  mahjong:
+    "和牌",
+
+  whoWon:
+    "谁赢了？",
+
+  howWon:
+    "如何和牌？",
+
+  whoDiscarded:
+    "谁放铳？",
+
+  saveHand:
+    "保存本局",
+
+  activeGameTitle:
+    "你有一场进行中的对局",
+
+  activeGameMessage:
+    "你已经有一场保存的对局。\n你想做什么？",
+
+  cancelAndNewGame:
+    "取消并开始新对局",
+
+  noSavedGame:
+    "没有保存的对局。",
+
+  gameSavedMessage:
+    "对局将被保存，你可以稍后继续。",
+
+  undoConfirmation:
+    "确定要撤销上一局吗？",
+
+  undoMessage:
+    "上一局的分数将被删除，你可以重新记录这一局。",
+
+  noHandsToUndo:
+    "没有可以撤销的局。",
+
+  finishGameQuestion:
+    "结束对局？",
+
+  finishGameMessage:
+    "对局将被标记为已结束，并添加到历史记录中。",
+
+  gameFinished:
+    "对局已结束",
+
+  gameFinishedMessage:
+    "对局已成功结束。",
+
+  cannotRegisterFinished:
+    "对局已经结束，无法继续记录新的局。",
+
+  cannotSaveGame:
+    "无法保存对局。",
+
+  cannotSaveHistory:
+    "无法将对局保存到历史记录。",
+
+  windChange:
+    "换风",
+
+  roundChange:
+    "进入第",
+
+  newPositions:
+    "新的位置",
+
+  windSouth:
+    "南风",
+
+  windWest:
+    "西风",
+
+  windNorth:
+    "北风",
+
+  windEast:
+    "东风",
+
+  east: "东",
+  south: "南",
+  west: "西",
+  north: "北",
+  na: "N/A",
+
+  continueButton:
+    "继续",
+
+  rest:
+    "休息",
+
+  inactivePlayer:
+    "未参与玩家",
+
+  results:
+    "结果",
+
+  ranking:
+    "排名",
+
+  winnerLabel:
+    "赢家",
+
+  first: "第1名",
+  second: "第2名",
+  third: "第3名",
+  fourth: "第4名",
+
+  playedHands:
+    "已进行局数",
+
+  noHandsRegistered:
+    "还没有记录任何局。",
+
+  backToHistory:
+    "返回历史记录",
+
+  historyTitle:
+    "对局历史",
+
+  noFinishedGames:
+    "没有已结束的对局。",
+
+  game:
+    "对局",
+
+  players:
+    "玩家",
+
+  playersUnavailable:
+    "未参与玩家",
+
+  noPlayerData:
+    "没有玩家数据",
+
+  viewGame:
+    "查看对局",
+
+  deleteGame:
+    "删除",
+
+  deleteGameConfirmation:
+    "确定要从历史记录中删除这场对局吗？",
+
+  gameDate:
+    "日期",
+
+  copyright:
+    "© 2026 C. Horcajo · 版权所有。"
+};
+
+export default zh;
