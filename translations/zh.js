bf6386ba2ce4e65e9aef3e87a6ce69570df0fd1f
cintiaMahjong <cintiahor@gmail.com>
2026-09-04 10:39:11 +0200

    Update zh.js

--- translations/zh.js
+++ translations/zh.js
@@ -1,226 +1,263 @@
 const zh = {
-  language: "语言",
-  spanish: "西班牙语",
-  english: "英语",
+
+  language: "語言",
+
+  spanish: "西班牙語",
+
+  english: "英語",
+
   chinese: "中文",
 
-  appTitle: "马德里麻将",
-  appSubtitle: "麻将对局管理器 · 4或5名玩家",
+  appTitle: "馬德里麻將",
 
-  continueGame: "继续对局",
-  newGame: "新对局",
-  history: "对局历史",
+  appSubtitle: "麻將對局管理器 · 4或5名玩家",
 
-  newGameTitle: "新对局",
-  numberOfPlayers: "玩家人数",
+  continueGame: "繼續對局",
+
+  newGame: "新對局",
+
+  history: "對局歷史",
+
+  newGameTitle: "新對局",
+
+  numberOfPlayers: "玩家人數",
+
   fourPlayers: "4名玩家",
+
   fivePlayers: "5名玩家",
 
   player: "玩家",
-  playerName: "玩家姓名",
-  enterPlayerName: "输入姓名",
-  enterPlayers: "请输入所有玩家的姓名",
-  emptyPlayerNames:
-    "请输入所有玩家的姓名。",
 
-  startGame: "开始对局",
+  playerName: "玩家姓名",
+
+  enterPlayerName: "輸入姓名",
+
+  enterPlayers: "請輸入所有玩家的姓名",
+
+  emptyPlayerNames:
+    "請輸入所有玩家的姓名。",
+
+  startGame: "開始對局",
+
   back: "返回",
 
   round: "圈",
+
   hand: "局",
+
   of: "/",
 
-  registerHand: "记录本局",
-  undoHand: "撤销本局",
-  saveAndExit: "保存并退出",
-  finishGame: "结束对局",
+  registerHand: "記錄本局",
+
+  undoHand: "撤銷本局",
+
+  saveAndExit: "保存並退出",
+
+  finishGame: "結束對局",
 
   wall: "自摸",
-  discard: "荣和",
+
+  discard: "榮和",
+
   draw: "流局",
 
-  winner: "赢家",
-  loser: "输家",
-  discarder: "放铳玩家",
+  winner: "贏家",
+
+  loser: "輸家",
+
+  discarder: "放銃玩家",
 
   points: "分",
-  handPoints: "本局分数",
+
+  handPoints: "本局分數",
+
   minimum8: "最低8分",
 
   enterPoints:
-    "请输入分数。",
+    "請輸入分數。",
 
   pointsMustBeNumber:
-    "分数必须是数字。",
+    "分數必須是數字。",
 
   minimumPoints:
     "本局至少需要8分。",
 
   invalidPointsTitle:
-    "分数无效",
+    "分數無效",
 
   accept:
-    "确定",
+    "確定",
 
-  confirm: "确认",
+  confirm: "確認",
+
   cancel: "取消",
+
   next: "下一步",
+
   previous: "上一步",
 
   registerHandTitle:
-    "记录本局",
+    "記錄本局",
 
   mahjong:
     "和牌",
 
   whoWon:
-    "谁赢了？",
+    "誰贏了？",
 
   howWon:
     "如何和牌？",
 
   whoDiscarded:
-    "谁放铳？",
+    "誰放銃？",
 
   saveHand:
     "保存本局",
 
   activeGameTitle:
-    "你有一场进行中的对局",
+    "你有一場進行中的對局",
 
   activeGameMessage:
-    "你已经有一场保存的对局。\n你想做什么？",
+    "你已經有一場保存的對局。\n你想做什麼？",
 
   cancelAndNewGame:
-    "取消并开始新对局",
+    "取消並開始新對局",
 
   noSavedGame:
-    "没有保存的对局。",
+    "沒有保存的對局。",
 
   gameSavedMessage:
-    "对局将被保存，你可以稍后继续。",
+    "對局將被保存，你可以稍後繼續。",
 
   undoConfirmation:
-    "确定要撤销上一局吗？",
+    "確定要撤銷上一局嗎？",
 
   undoMessage:
-    "上一局的分数将被删除，你可以重新记录这一局。",
+    "上一局的分數將被刪除，你可以重新記錄這一局。",
 
   noHandsToUndo:
-    "没有可以撤销的局。",
+    "沒有可以撤銷的局。",
 
   finishGameQuestion:
-    "结束对局？",
+    "結束對局？",
 
   finishGameMessage:
-    "对局将被标记为已结束，并添加到历史记录中。",
+    "對局將被標記為已結束，並添加到歷史記錄中。",
 
   gameFinished:
-    "对局已结束",
+    "對局已結束",
 
   gameFinishedMessage:
-    "对局已成功结束。",
+    "對局已成功結束。",
 
   cannotRegisterFinished:
-    "对局已经结束，无法继续记录新的局。",
+    "對局已經結束，無法繼續記錄新的局。",
 
   cannotSaveGame:
-    "无法保存对局。",
+    "無法保存對局。",
 
   cannotSaveHistory:
-    "无法将对局保存到历史记录。",
+    "無法將對局保存到歷史記錄。",
 
   windChange:
-    "换风",
+    "換風",
 
   roundChange:
-    "进入第",
+    "進入第",
 
   newPositions:
     "新的位置",
 
   windSouth:
-    "南风",
+    "南風",
 
   windWest:
-    "西风",
+    "西風",
 
   windNorth:
-    "北风",
+    "北風",
 
   windEast:
-    "东风",
+    "東風",
 
-  east: "东",
+  east: "東",
+
   south: "南",
+
   west: "西",
+
   north: "北",
+
   na: "N/A",
 
   continueButton:
-    "继续",
+    "繼續",
 
   rest:
     "休息",
 
   inactivePlayer:
-    "未参与玩家",
+    "未參與玩家",
 
   results:
-    "结果",
+    "結果",
 
   ranking:
     "排名",
 
   winnerLabel:
-    "赢家",
+    "贏家",
 
   first: "第1名",
+
   second: "第2名",
+
   third: "第3名",
+
   fourth: "第4名",
 
   playedHands:
-    "已进行局数",
+    "已進行局數",
 
   noHandsRegistered:
-    "还没有记录任何局。",
+    "還沒有記錄任何局。",
 
   backToHistory:
-    "返回历史记录",
+    "返回歷史記錄",
 
   historyTitle:
-    "对局历史",
+    "對局歷史",
 
   noFinishedGames:
-    "没有已结束的对局。",
+    "沒有已結束的對局。",
 
   game:
-    "对局",
+    "對局",
 
   players:
     "玩家",
 
   playersUnavailable:
-    "未参与玩家",
+    "未參與玩家",
 
   noPlayerData:
-    "没有玩家数据",
+    "沒有玩家資料",
 
   viewGame:
-    "查看对局",
+    "查看對局",
 
   deleteGame:
-    "删除",
+    "刪除",
 
   deleteGameConfirmation:
-    "确定要从历史记录中删除这场对局吗？",
+    "確定要從歷史記錄中刪除這場對局嗎？",
 
   gameDate:
     "日期",
 
   copyright:
-    "© 2026 C. Horcajo · 版权所有。"
+    "© 2026 C. Horcajo · 版權所有。"
+
 };
 
 export default zh;
