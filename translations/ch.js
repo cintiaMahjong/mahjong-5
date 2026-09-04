b95a07a14a067858ff988f6af405d5960cde99c8
cintiaMahjong <cintiahor@gmail.com>
2026-09-04 16:24:48 +0200

    Add Chinese translations for the application

---
+++ translations/ch.js
@@ -0,0 +1,293 @@
+const zh = {
+
+  language: "语言",
+
+  spanish: "西班牙语",
+
+  english: "英语",
+
+  chinese: "中文",
+
+  appTitle: "Mahjong Madrid",
+
+  appSubtitle: "MCR 对局管理器 · 4或5名玩家",
+
+  continueGame: "继续对局",
+
+  newGame: "新对局",
+
+  history: "对局历史",
+
+  statistics: "统计",
+
+  newGameTitle: "新对局",
+
+  numberOfPlayers: "玩家人数",
+
+  fourPlayers: "4名玩家",
+
+  fivePlayers: "5名玩家",
+
+  player: "玩家",
+
+  playerName: "玩家姓名",
+
+  enterPlayerName: "请输入姓名",
+
+  enterPlayers: "请输入所有玩家的姓名",
+
+  emptyPlayerNames:
+    "请输入所有玩家的姓名。",
+
+  startGame: "开始对局",
+
+  back: "返回",
+
+  round: "回合",
+
+  hand: "局",
+
+  of: "共",
+
+  registerHand: "记录本局",
+
+  undoHand: "撤销本局",
+
+  saveAndExit: "保存并退出",
+
+  finishGame: "结束对局",
+
+  wall: "自摸",
+
+  discard: "点和",
+
+  draw: "和局",
+
+  winner: "赢家",
+
+  loser: "输家",
+
+  discarder: "放铳玩家",
+
+  points: "分",
+
+  handPoints: "本局分数",
+
+  minimum8: "至少8分",
+
+  enterPoints: "请输入分数。",
+
+  pointsMustBeNumber:
+    "分数必须是数字。",
+
+  minimumPoints:
+    "本局至少需要8分。",
+
+  invalidPointsTitle: "无效分数",
+
+  accept: "接受",
+
+  confirm: "确认",
+
+  cancel: "取消",
+
+  next: "下一步",
+
+  previous: "上一步",
+
+  registerHandTitle: "记录本局",
+
+  mahjong: "麻将",
+
+  whoWon: "谁赢了？",
+
+  howWon: "如何获胜？",
+
+  whoDiscarded: "谁放铳了？",
+
+  saveHand: "保存本局",
+
+  activeGameTitle:
+    "你有一场进行中的对局",
+
+  activeGameMessage:
+    "你已经有一场保存的对局。\n你想怎么做？",
+
+  cancelAndNewGame:
+    "取消并开始新对局",
+
+  noSavedGame:
+    "没有保存的对局。",
+
+  gameSavedMessage:
+    "对局将被保存，你可以稍后继续。",
+
+  undoConfirmation:
+    "确定要撤销上一局吗？",
+
+  undoMessage:
+    "上一局的分数将被删除，你可以重新记录这一局。",
+
+  noHandsToUndo:
+    "没有可以撤销的局。",
+
+  finishGameQuestion:
+    "结束对局？",
+
+  finishGameMessage:
+    "该对局将被标记为已结束，并出现在对局历史中。",
+
+  gameFinished:
+    "对局已结束",
+
+  gameFinishedMessage:
+    "对局已成功结束。",
+
+  cannotRegisterFinished:
+    "对局已经结束，无法继续记录新的局。",
+
+  cannotSaveGame:
+    "无法保存对局。",
+
+  cannotSaveHistory:
+    "无法将对局保存到历史记录。",
+
+  windChange:
+    "风位变更",
+
+  roundChange:
+    "进入第",
+
+  newPositions:
+    "新的风位",
+
+  windSouth:
+    "南风",
+
+  windWest:
+    "西风",
+
+  windNorth:
+    "北风",
+
+  windEast:
+    "东风",
+
+  east: "东",
+
+  south: "南",
+
+  west: "西",
+
+  north: "北",
+
+  na: "N/A",
+
+  continueButton:
+    "继续",
+
+  rest:
+    "休息",
+
+  inactivePlayer:
+    "非活跃玩家",
+
+  results:
+    "结果",
+
+  ranking:
+    "排名",
+
+  winnerLabel:
+    "赢家",
+
+  first: "第1名",
+
+  second: "第2名",
+
+  third: "第3名",
+
+  fourth: "第4名",
+
+  playedHands:
+    "已进行的局数",
+
+  noHandsRegistered:
+    "暂无记录的牌局。",
+
+  backToHistory:
+    "返回对局历史",
+
+  historyTitle:
+    "对局历史",
+
+  noFinishedGames:
+    "没有已结束的对局。",
+
+  game:
+    "对局",
+
+  players:
+    "玩家",
+
+  playersUnavailable:
+    "非活跃玩家",
+
+  noPlayerData:
+    "没有玩家数据",
+
+  viewGame:
+    "查看对局",
+
+  deleteGame:
+    "删除",
+
+  deleteGameConfirmation:
+    "确定要从对局历史中删除这场对局吗？",
+
+  gameDate:
+    "日期",
+
+  // =====================================================
+  // 统计
+  // =====================================================
+
+  statistics:
+    "统计",
+
+  statisticsPlayer:
+    "玩家",
+
+  statisticsGamesPlayed:
+    "已进行对局",
+
+  statisticsPositions:
+    "最终排名",
+
+  firstPlace:
+    "第1名",
+
+  secondPlace:
+    "第2名",
+
+  thirdPlace:
+    "第3名",
+
+  statisticsPoints:
+    "分数",
+
+  statisticsTotalPoints:
+    "累计分数",
+
+  statisticsAveragePoints:
+    "每局平均分数",
+
+  noStatistics:
+    "目前还没有已结束的对局可供统计。",
+
+  copyright:
+    "© 2026 C. Horcajo · 版权所有"
+
+};
+
+export default zh;
